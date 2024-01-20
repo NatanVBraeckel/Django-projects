44f757d3b24760a00003620607ca0f64f8e6e962
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-01-20 17:48:13 +0100

    Adding image field to API (73)

--- rest_movies/rest_movies/movies/models.py
+++ rest_movies/rest_movies/movies/models.py
@@ -10,3 +10,4 @@ class Moviedata(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     typ = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='images/', default='images/none/no_img.jpg')
