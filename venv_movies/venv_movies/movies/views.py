c3ecca404e77b3aa0a40be0ceca130ef6c2f5a59
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-01-21 13:49:50 +0100

    Adding paginator (78)

--- venv_movies/venv_movies/movies/views.py
+++ venv_movies/venv_movies/movies/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Movies
+from django.core.paginator import Paginator
 
 # Create your views here.
 def movie_list(request):
     list = Movies.objects.all()
-    return render(request, 'movies/movie_list.html', {'movie_objects': list})
+
+    paginator = Paginator(list, 4)
+    page = request.GET.get('page')
+    movie_objects = paginator.get_page(page)
+
+    return render(request, 'movies/movie_list.html', {'movie_objects': movie_objects})
