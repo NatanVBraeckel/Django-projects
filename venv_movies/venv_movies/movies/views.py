f2988226534f0cee890550c9a9feb8224bf7b2ed
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-01-21 13:57:16 +0100

    Adding search functionality (79)

--- venv_movies/venv_movies/movies/views.py
+++ venv_movies/venv_movies/movies/views.py
@@ -4,9 +4,13 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def movie_list(request):
-    list = Movies.objects.all()
+    movie_objects = Movies.objects.all()
 
-    paginator = Paginator(list, 4)
+    movie_name = request.GET.get('movie_name')
+    if movie_name != '' and movie_name is not None:
+        movie_objects = movie_objects.filter(name__icontains=movie_name)
+
+    paginator = Paginator(movie_objects, 4)
     page = request.GET.get('page')
     movie_objects = paginator.get_page(page)
 
