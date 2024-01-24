a806ead95949472efa07d18213a723fa2677fbd8
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-01-24 20:47:11 +0100

    Adding custom fields (114)

--- e_commerce/ecomsite/shop/admin.py
+++ e_commerce/ecomsite/shop/admin.py
@@ -5,6 +5,9 @@ admin.site.site_header = "E-commerce site"
 admin.site.site_title = "ABC Shopping"
 admin.site.index_title = "Manage ABC Shopping"
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'discount_price', 'category', 'description')
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
