5197ff8ebd094cc2b82489a960ebcd6a9154fe48
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-01-28 17:48:44 +0100

    Accepting URLs from users (136)

--- web_scraper/web_scraper/scraper/views.py
+++ web_scraper/web_scraper/scraper/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 import requests
+from django.http import HttpResponseRedirect
 from bs4 import BeautifulSoup
 from .models import Link
 
 # Create your views here.
 def scrape(request):
-    page = requests.get('https://www.google.com')
-    soup = BeautifulSoup(page.text, 'html.parser')
 
-    for link in soup.find_all('a'):
-        link_address = link.get("href")
-        link_text = link.string
-        Link.objects.create(address=link_address, name=link_text)
+    if request.method == "POST":
+        site = request.POST.get('site', '')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    link_address_list = Link.objects.all()
+        for link in soup.find_all('a'):
+            link_address = link.get("href")
+            link_text = link.string
+            Link.objects.create(address=link_address, name=link_text)
+        return HttpResponseRedirect('/')
+    else:
+        link_address_list = Link.objects.all()
 
     return render(request, 'scraper/result.html', {'link_address_list': link_address_list})
+
+def clear(request):
+    Link.objects.all().delete()
+    return HttpResponseRedirect('/')
