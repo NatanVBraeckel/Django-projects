358f4d33d21091c3496daac455f0bbd87189bb1d
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-01-28 19:48:07 +0100

    User functionality to add food (142)

--- calorie_tracker/calorie_tracker/tracker/views.py
+++ calorie_tracker/calorie_tracker/tracker/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Consume, Food
 
 # Create your views here.
 def index(request):
+
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume_object = Consume(user=user, food_consumed=consume)
+        consume_object.save()
+
     foods = Food.objects.all()
 
     return render(request, 'tracker/index.html', { 'foods': foods })
