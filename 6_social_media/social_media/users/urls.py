f2ce4739893802274bd597ef18c3f3e414ad6c85
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-04 16:45:09 +0100

    Password reset done view (163)

--- 6_social_media/social_media/users/urls.py
+++ 6_social_media/social_media/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name="users/password_change_form.html"), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset_form.html"), name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
 ]
