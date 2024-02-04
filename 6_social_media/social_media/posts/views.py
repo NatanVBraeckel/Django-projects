c21facb237e0ee4d8954a2a5a2d3452a5c42fcb8
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-04 21:13:50 +0100

    Creating post (172)

--- 6_social_media/social_media/posts/views.py
+++ 6_social_media/social_media/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import PostCreateForm
+
 
 # Create your views here.
+def post_create(request):
+    if request.method == "POST":
+        form_data = PostCreateForm(data=request.POST)
+        if form_data.is_valid():
+            new_item = form_data.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+    else:
+        form_data = PostCreateForm(data=request.POST)
+    return render(request, 'posts/create.html', {'form': form_data})
