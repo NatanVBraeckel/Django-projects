edeafb4692ed9f0e20490d931ff0f90ae15a9ce7
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-04 21:17:21 +0100

    Submitting post part 1 (173)

--- 6_social_media/social_media/posts/views.py
+++ 6_social_media/social_media/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def post_create(request):
     if request.method == "POST":
         form_data = PostCreateForm(data=request.POST)
