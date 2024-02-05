6784646a7a2be77b97d44bd5cad78d23dc51a48d
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-05 14:25:06 +0100

    Comment functionality part 5 (200)

--- 6_social_media/social_media/posts/models.py
+++ 6_social_media/social_media/posts/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    body = models.TextField()
+    body = models.CharField(max_length=128)
     created = models.DateTimeField(auto_now=True)
 
     class Meta:
