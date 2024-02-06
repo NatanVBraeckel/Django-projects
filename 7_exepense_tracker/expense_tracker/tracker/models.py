69b0b6b4eefb65c95ed7d7a58eb60bfb990e6144
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-06 10:48:22 +0100

    Creating admin user (204)

--- 7_exepense_tracker/expense_tracker/tracker/models.py
+++ 7_exepense_tracker/expense_tracker/tracker/models.py
@@ -6,3 +6,6 @@ class Expense(models.Model):
     amount = models.IntegerField()
     category = models.CharField(max_length=50)
     data = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
