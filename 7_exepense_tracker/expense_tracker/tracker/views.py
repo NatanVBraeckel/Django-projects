cb2363906154b06382cd00fb0285a824c8f42473
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-06 11:03:50 +0100

    Creating expense form (208)

--- 7_exepense_tracker/expense_tracker/tracker/views.py
+++ 7_exepense_tracker/expense_tracker/tracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'tracker/index.html')
+    form = ExpenseForm()
+    return render(request, 'tracker/index.html', { 'expense_form': form })
