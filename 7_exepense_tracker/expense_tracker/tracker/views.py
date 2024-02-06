754e480825bf63997b4a9d7f89ab2f999179b03c
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-06 11:11:56 +0100

    Handling POST request (210)

--- 7_exepense_tracker/expense_tracker/tracker/views.py
+++ 7_exepense_tracker/expense_tracker/tracker/views.py
@@ -3,5 +3,11 @@ from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
+
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
     form = ExpenseForm()
     return render(request, 'tracker/index.html', { 'expense_form': form })
