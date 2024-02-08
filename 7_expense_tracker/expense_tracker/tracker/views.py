0d5912b971348128bfc3a9959e71b755e657fabe
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-08 09:33:43 +0100

    Total accross week and year (and month) (221)

--- 7_expense_tracker/expense_tracker/tracker/views.py
+++ 7_expense_tracker/expense_tracker/tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
 from django.db.models import Sum
+import datetime
 
 # Create your views here.
 def index(request):
@@ -14,8 +15,24 @@ def index(request):
     expenses = Expense.objects.all()
     total_amount = expenses.aggregate(Sum('amount'))
     # print(total_amount)
+
+    # 365 days expenses
+    last_year = datetime.today() - datetime.timedelta(days=365)
+    dates = Expense.objects.filter(data_gt=last_year)
+    total_year = dates.aggregate(Sum('amount'))
+
+    # last month expenses
+    last_month = datetime.today() - datetime.timedelta(days=30)
+    dates = Expense.objects.filter(data_gt=last_month)
+    total_month = dates.aggregate(Sum('amount'))
+
+    # last week expenses
+    last_week = datetime.today() - datetime.timedelta(days=7)
+    dates = Expense.objects.filter(data_gt=last_week)
+    total_week = dates.aggregate(Sum('amount'))
+
     form = ExpenseForm()
-    return render(request, 'tracker/index.html', { 'expense_form': form, 'expenses': expenses, 'total_amount': total_amount })
+    return render(request, 'tracker/index.html', { 'expense_form': form, 'expenses': expenses, 'total_amount': total_amount, 'total_year': total_year, 'total_month': total_month, 'total_week': total_week })
 
 def edit(request, id):
 
