c4e757ef4a5cf48f7c2d8c476da7fe63b8ad0549
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 16:58:55 +0100

    Finding cumulative sales for products (301)

--- 9_digital_marketplace/digital_marketplace/marketplace/views.py
+++ 9_digital_marketplace/digital_marketplace/marketplace/views.py
@@ -129,4 +129,7 @@ def sales(request):
     #everday sum for the past 30 days
     daily_sales_sums = OrderDetail.objects.filter(product__seller=request.user).values('created_on__date').order_by('created_on__date').annotate(sum=Sum('amount'))
 
-    return render(request, 'marketplace/sales.html', { 'total_sales': total_sales, 'yearly_sales': yearly_sales, 'monthly_sales': monthly_sales, 'weekly_sales': weekly_sales, 'daily_sales_sums': daily_sales_sums })
+    #everday sum for each product
+    product_sales_sums = OrderDetail.objects.filter(product__seller=request.user).values('product__name').order_by('product__name').annotate(sum=Sum('amount'))
+
+    return render(request, 'marketplace/sales.html', { 'total_sales': total_sales, 'yearly_sales': yearly_sales, 'monthly_sales': monthly_sales, 'weekly_sales': weekly_sales, 'daily_sales_sums': daily_sales_sums, 'product_sales_sums': product_sales_sums })
