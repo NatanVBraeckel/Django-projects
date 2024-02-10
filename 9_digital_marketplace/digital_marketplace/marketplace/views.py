b4e55300b151db80fffc26608bdec42169339444
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 14:08:09 +0100

    Creating order in backend (270) + Payment success view (271)

--- 9_digital_marketplace/digital_marketplace/marketplace/views.py
+++ 9_digital_marketplace/digital_marketplace/marketplace/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, reverse
+from .models import Product,OrderDetail
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+import stripe,json
+import random
 
 # Create your views here.
 def index(request):
@@ -9,3 +13,28 @@ def index(request):
 def detail(request, id):
     product = Product.objects.get(id=id)
     return render(request, 'marketplace/detail.html', { 'product': product })
+
+# @csrf_exempt
+def create_checkout_session(request,id):
+    # ik ga niet de stripe functionaliteit inbouwen
+
+    request_data = json.loads(request.body)
+    product = Product.objects.get(id=id)
+
+    if(random.uniform(0, 1) == 1):
+        return render(request, 'marketplace/index.html')
+    
+    order = OrderDetail()
+    order.customer_email = request_data['email']
+    order.product = product
+    order.amount = int(product.price)
+    order.save()
+    
+    return render(request, 'marketplace/index.html')
+
+def payment_success_view(request, id):
+    order = OrderDetail.objects.get(id=id)
+    order.has_paid = True
+    order.save()
+
+    return render(request, 'marketplace/payment_success.html', { 'order': order })
