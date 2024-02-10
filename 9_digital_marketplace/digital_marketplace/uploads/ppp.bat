898e15477175dda3db797584e511d119639ff015
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 13:25:06 +0100

    Displaying products (261)

---
+++ 9_digital_marketplace/digital_marketplace/uploads/ppp.bat
@@ -0,0 +1,6 @@
+@echo off
+start explorer "C:\Users\natan\School\School_2023-2024\Semester1\PPP\Github rep\Django-projects"
+start "" "https://www.udemy.com/home/my-courses/learning/"
+start "" "http://127.0.0.1:8000/"
+start "" "http://127.0.0.1:8000/admin"
+start "" "https://github.com/NatanVBraeckel/Django-projects"
