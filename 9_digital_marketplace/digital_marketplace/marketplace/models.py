be60bc055b8c29033d44ec95878cffccbe87e15b
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 16:07:00 +0100

    Modifying the product model (291)

--- 9_digital_marketplace/digital_marketplace/marketplace/models.py
+++ 9_digital_marketplace/digital_marketplace/marketplace/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -6,6 +7,7 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
     price = models.FloatField()
     file = models.FileField(upload_to='uploads')
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
