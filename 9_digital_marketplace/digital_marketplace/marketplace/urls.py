a86722db0de3b7f76637de85f4f939a55bfad342
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 16:04:45 +0100

    Logout functionality (290)

--- 9_digital_marketplace/digital_marketplace/marketplace/urls.py
+++ 9_digital_marketplace/digital_marketplace/marketplace/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('dashboard', views.dashboard, name="dashboard"),
     path('register', views.register, name="register"),
     path('login', auth_views.LoginView.as_view(template_name='marketplace/login.html'), name="login"),
+    path('logout', auth_views.LogoutView.as_view(template_name='marketplace/logout.html'), name="logout"),
 ]
