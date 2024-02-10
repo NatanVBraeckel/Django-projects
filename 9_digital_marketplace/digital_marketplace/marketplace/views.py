50c0245d44d92364072728bcff042f4f837367d1
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 13:34:39 +0100

    Detail view (263)

--- 9_digital_marketplace/digital_marketplace/marketplace/views.py
+++ 9_digital_marketplace/digital_marketplace/marketplace/views.py
@@ -5,3 +5,7 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'marketplace/index.html', { 'products': products })
+
+def detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'marketplace/detail.html', { 'product': product })
