59eb8661050237a3bda3d9b3cda7d57557e4038d
NatanVBraeckel <r0804023@student.thomasmore.be>
2024-02-10 15:59:25 +0100

    Login functionality

--- 9_digital_marketplace/digital_marketplace/marketplace/urls.py
+++ 9_digital_marketplace/digital_marketplace/marketplace/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('delete/<int:id>', views.product_delete, name="delete"),
     path('dashboard', views.dashboard, name="dashboard"),
     path('register', views.register, name="register"),
+    path('login', auth_views.LoginView.as_view(template_name='marketplace/login.html'), name="login"),
 ]
